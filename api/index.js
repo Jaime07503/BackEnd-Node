ac1c0f2d15e9ff9bc1264d1e9e7cb50cb3c63f87
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-11-17 20:39:47 -0600

    Add all url

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const port = process.env.port || 3001;
 
 var corsOptions = {
   origin: [
+    "*",
     "https://invitacion-graduacion-sofy.vercel.app/",
     "https://invitacion-graduacion-sofy.vercel.app/invitacion/",
   ],
